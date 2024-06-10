32eafd5802125400964b33985beb637141f37b96
TeenaRaghav <teenaraghav3017@gmail.com>
2024-06-10 13:23:30 -0400

    updated code

--- src/test/java/com/cucumber/runner/Runner.java
+++ src/test/java/com/cucumber/runner/Runner.java
@@ -11,8 +11,10 @@ import io.cucumber.junit.CucumberOptions;
 				 glue = {"com.cucumber.steps"},
 				 plugin= {"pretty", "html:target/cucumber-reports/cucumber.html",
 						 "json:target/cucumber-reports/cucumber.json"},
-				 monochrome= true,
-				 dryRun= false
+				 monochrome= true,//it is controls whether console output is colorful and readable. When set to true, monochrome prints console output without color, 
+				 //making it more readable. When set to false, which is the default value, monochrome prints console output with color, but some characters may be unreadable.
+				 dryRun= false// option that compiles feature files and step definitions, and verifies that each step in the feature file has a corresponding step definition in the step definition file. The dry run option can be set to true or false, with the default value being false.
+				 //When set to true, Cucumber will verify individual steps in the feature file and the implementation code of steps in the feature file within the step definition file
 		
 		)
 
